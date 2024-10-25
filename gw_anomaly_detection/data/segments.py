a8bf958a4518de1d45d9189644b59962b72aa3fa
Chia-Jui Chou <chiajuichou@nycu.edu.tw>
2024-10-26 06:31:56 +0800

    Adding asds.

--- gw_anomaly_detection/data/segments.py
+++ gw_anomaly_detection/data/segments.py
@@ -1,14 +1,62 @@
 #!/bin/python
 import h5py
+from gwpy.segments import Segment
 from gwpy.segments import SegmentList
 
 def get_background_segments(
         ifo: str,
-        segment_file: str,
+        segment_files: list,
         glitch_info_file: str,
-        glitch_window_length: float=2,
+        data_cache: str,
+        start: float,
+        end: float,
+        glitch_window_length: float=4,
 ):
+    # Get science segments with start and end time.
+    loaded_segs = SegmentList([])
+    for file in segment_files:
+        loaded_segs.extend(SegmentList.read(file, format='segwizard'))
+
+    target_seg = Segment(start, end)
+    selected_segs = SegmentList([])
+    for seg in loaded_segs:
+        if target_seg.intersects(seg):
+            selected_segs.append(target_seg & seg)
+
+    selected_segs.sort()
+
+    # Get the trigger times of the glitches.
     with h5py.File(glitch_info_file, 'r') as f:
         glitch_times = f['glitch_info']['time'][:]
 
-    return glitch_times
+    selected_times = []
+    for time in glitch_times:
+        for seg in selected_segs:
+            if time >= seg.start and time <= seg.end:
+                selected_times.append(time)
+
+    selected_times.sort()
+
+    # Get background segments without the glitches.
+    bg_segs = SegmentList([])
+    seg_id = 0
+    to_new_seg = True
+    while len(selected_times) != 0:
+        seg_st = selected_segs[seg_id].start
+        seg_ed = selected_segs[seg_id].end
+        if to_new_seg:
+            previous = seg_st
+        time = selected_times.pop(0)
+        if (time - seg_st >= 0 and time - seg_ed <=0):
+            bg_segs.append(Segment(previous, time))
+            previous = time
+            to_new_seg = False
+        if time - seg_ed > 0:
+            bg_segs.append(Segment(previous, seg_ed))
+            selected_times.insert(0, time)
+            to_new_seg = True
+            seg_id += 1
+
+    bg_segs.write("./bg_segs.segwizard", format="segwizard")
+
+    return bg_segs
--- test.py
+++ test.py
@@ -2,15 +2,31 @@
 from gw_anomaly_detection.data.segments import get_background_segments
 
 def main():
-    ifo = 'L1'
-    segment_file = f"/home/chiajui/gw-anomaly-detection/segments/{ifo}_science_segs.segwizard"
+    ifos = ['H1', 'L1']
+    ifo = ifos[0]
+    segment_files = [
+        f"/home/chiajui/gw-anomaly-detection/segments/O3a/{ifo}_science_segs.segwizard",
+    ]
     glitch_info_file = f"/home/chiajui/gw-anomaly-detection/glitch_info/O3a/{ifo}_glitch_info.hdf5"
+    data_cache = f"O3_data{ifo}"
+    interval = {
+        'H1': (1238166018, 1238856499),
+        'L1': (1238166018, 1238772909),
+    }
+    start = interval[ifo][0]
+    end = interval[ifo][1]
+
     segs = get_background_segments(
         ifo=ifo,
-        segment_file=segment_file,
+        segment_files=segment_files,
         glitch_info_file=glitch_info_file,
+        data_cache=data_cache,
+        start=start,
+        end=end,
+        glitch_window_length=4,
     )
-    print(segs.shape)
+    print(len(segs))
+    print(segs[0])
 
 if __name__ == "__main__":
     main()
