3e25e2c0fa454f3ec2c3d29d8b8e74b245d5ad13
Chia-Jui Chou <chiajuichou@nycu.edu.tw>
2024-11-04 14:06:55 +0800

    Adding processing.py

---
+++ processing.py
@@ -0,0 +1,17 @@
+#!/bin/python
+import os
+from gw_anomaly_detection.data.segments import segment_info
+from gw_anomaly_detection.data.s3_utils import s3_session
+
+# Loading the information of the target segments.
+print(os.getcwd())
+
+# Loading strain and asd into data cache.
+
+# Waveform injection.
+
+# Processing data.
+
+# Writing processed data to s3 buckets.
+
+# Gathering data.
