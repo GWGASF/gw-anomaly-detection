626295b65a22bbac49187de0ff28b695cf2a6ab2
dfredin <dfredin@uw.edu>
2025-03-19 22:15:49 -0700

    Preparing for submodule GASF

--- Running_injection_and_training.sh
+++ Running_injection_and_training.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
-cd /opt/repo/gw-anomaly-detection
+cd /home/dfredin/repos/gw-anomaly-detection
 mkdir -p /home/app/test_data/glitch /home/app/test_data/noise /home/app/test_data/bbh /home/app/test_data/lfsg /home/app/test_data/hfsg /home/app/test_data/test
 
-python /opt/repo/gw-anomaly-detection/get_segments.py
-python /opt/repo/gw-anomaly-detection/cli.py --ptype test
-python /opt/repo/gw-anomaly-detection/cli.py --sid 0 --eid 500 --ptype glitch
-python /opt/repo/gw-anomaly-detection/cli.py --sid 500 --eid 1000 --ptype background
-python /opt/repo/gw-anomaly-detection/cli.py --sid 1000 --eid 1500 --ptype injection --itype bbh
-python /opt/repo/gw-anomaly-detection/cli.py --sid 1500 --eid 2000 --ptype injection --itype hfsg
-python /opt/repo/gw-anomaly-detection/cli.py --sid 2000 --eid 2500 --ptype injection --itype lfsg
+python /home/dfredin/repos/gw-anomaly-detection/get_segments.py
+# python /home/dfredin/repos/gw-anomaly-detection/cli.py --ptype test
+python /home/dfredin/repos/gw-anomaly-detection/cli.py --sid 0 --eid 500 --ptype glitch
+python /home/dfredin/repos/gw-anomaly-detection/cli.py --sid 500 --eid 1000 --ptype background
+python /home/dfredin/repos/gw-anomaly-detection/cli.py --sid 1000 --eid 1500 --ptype injection --itype bbh
+# python /home/dfredin/repos/gw-anomaly-detection/cli.py --sid 1500 --eid 2000 --ptype injection --itype hfsg
+# python /home/dfredin/repos/gw-anomaly-detection/cli.py --sid 2000 --eid 2500 --ptype injection --itype lfsg
 
-python /opt/repo/gw-anomaly-detection/run_pipeline.py
+python /home/dfredin/repos/gwgasf/src/main.py --newgasf --nbbh 50 --nbg 50 --nglitch 50
+python /home/dfredin/repos/gwgasf/src/main.py --train --nbbh 50 --nbg 50 --nglitch 50 --epoch 5 --batch 1
